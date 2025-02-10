3bbaab60ae87d97ec00b6239b7b1301d978fef3e
denis <Denisik33@yandex.ru>
2025-02-10 14:54:35 +0300

    Cделано ДЗ Опера 2ч. ver3.0

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,16 @@ public class Main {
         int clientOs = 1;
         if (clientOs == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
+        }else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         //Задание 2
         int clientDeviceYear = 2015;
-        if (clientOs == 0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }
-        } else if (clientOs == 1) {
-            if (clientDeviceYear < 2015) {
+        if (clientOs == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear < 2015) {
+            {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else if (clientOs == 1 && clientDeviceYear >= 2015) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-
             }
 
         }
