d3521537f6ba415e5b8abeb3cd9039f03fc17408
denis <Denisik33@yandex.ru>
2025-02-24 18:38:04 +0300

    Cделано ДЗ Массивы ч.2 ver.2.1

--- src/Main.java
+++ src/Main.java
@@ -22,16 +22,16 @@ public class Main {
                 maxExpenses = expenses;
             }
             if (expenses < minExpenses) {
-                minExpenses =expenses ;
+                minExpenses = expenses;
             }
         }
-        System.out.println("Минимальная сумма трат за неделю составила "+ minExpenses+" рублей");
-        System.out.println("Максимальная сумма трат за неделю составила "+maxExpenses+" рублей");
+        System.out.println("Минимальная сумма трат за неделю составила " + minExpenses + " рублей");
+        System.out.println("Максимальная сумма трат за неделю составила " + maxExpenses + " рублей");
         //Задание 3
-        int arithmeticMean=sum/5;
+        int arithmeticMean = sum / 5;
         System.out.println(arithmeticMean);
 //Задание 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = 0; i < reverseFullName.length; i++) {
         }
         for (int i = 0; i < reverseFullName.length / 2; i++) {
@@ -39,7 +39,7 @@ public class Main {
             reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
             reverseFullName[reverseFullName.length - 1 - i] = temp;
         }
-        for (int i = 0; i <reverseFullName.length; i++) {
+        for (int i = 0; i < reverseFullName.length; i++) {
             System.out.print(reverseFullName[i] + " ");
         }
 
