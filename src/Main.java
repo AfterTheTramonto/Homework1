55031eee7dd130b20edce5cad4bcc4a8168cea9e
denis <Denisik33@yandex.ru>
2025-03-04 23:30:15 +0300

    Cделано ДЗ Методы ver.2.0

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,64 @@
 import java.time.LocalDate;
 
 public class Main {
-    public static void yearCount(int a) {
-        if (a < 1584 || a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
-            System.out.println(a + " год - високосный");
+    public static void searchLeapYear(int year) {
+        if (year > 1584 || year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год - високосный");
         } else {
-            System.out.println(a + " год - не високосный год");
+            System.out.println(year + " год - не високосный год");
         }
     }
 
 
-    public static void getLink(int a, int b) {
-        if (a == 0 && b < 2015) {
+    public static void showingWhatVersion(int deviseOS, int currentYear) {
+        if (deviseOS == 0 && currentYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (a == 0) {
+        } else if (deviseOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (a == 1 && b < 2015) {
+        } else if (deviseOS == 1 && currentYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-    public static void calculationDelivery(int q, int e){
-        if (q < 20) {
-            e = 1;
-            System.out.println("Потребуется день " + e);
-        } else if (q > 20 && q < 60) {
-           e += 1;
-            System.out.println("Потребуется дней " + e);
-        } else if (q >= 60 && q <= 100) {
-            e += 2;
-            System.out.println("Потребуется дней " + e);
+
+    public static int countingDeliveryDays(int distance, int days) {
+        if (distance < 20) {
+            days = 1;
+            return 1;
+        } else if (distance > 20 && distance < 60) {
+            days += 1;
+            return 2;
+        } else if (distance >= 60 && distance <= 100) {
+            days += 2;
+            return 3;
         } else {
-            System.out.println("Доставки нет");}
+            throw new RuntimeException("Нет доставки");
+        }
     }
+
+    public static void printingDelivery(int daysReturn) {
+        if (daysReturn == 1) {
+            System.out.println("Потребуется дней: " + daysReturn);
+        } else if (daysReturn == 2) {
+            System.out.println("Потребуется дней: " + daysReturn);
+        } else if (daysReturn == 3) {
+            System.out.println("Потребуется дней: " + daysReturn);
+        }
+
+    }
+
 
     public static void main(String[] args) {
         //Задание 1
         int currentYear = LocalDate.now().getYear();
-        yearCount(currentYear);
-//Задание 2
+        searchLeapYear(currentYear);
+        //Задание 2
         int deviseOS = 1;
-        getLink(deviseOS, currentYear);
+        showingWhatVersion(deviseOS, currentYear);
         //Задание 3
         int deliveryDistance = 100;
         int deliveryDay = 1;
-        calculationDelivery(deliveryDistance,deliveryDay);
+        printingDelivery(countingDeliveryDays(deliveryDistance, deliveryDay));
     }
 }
