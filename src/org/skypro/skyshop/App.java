382f4e86b8760d1966b70cd2698035441cd087e3
denis <Denisik33@yandex.ru>
2025-04-14 18:08:03 +0300

    Пот и Кровь

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,41 +1,37 @@
 package org.skypro.skyshop;
 
+
 import org.skypro.skyshop.basket.ProductBasket;
-import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.*;
 
 public class App {
     public static void main(String[] args) {
-        Product apple = new Product("Яблоко", 50);
-        Product banana = new Product("Банан", 30);
-        Product orange = new Product("Апельсин", 70);
-        Product pear = new Product("Груша", 45);
-        Product kiwi = new Product("Киви", 60);
-        Product mango = new Product("Манго", 100);
-
         ProductBasket basket = new ProductBasket();
 
-        basket.addProduct(apple);
-        basket.addProduct(banana);
-        basket.addProduct(orange);
-        basket.addProduct(pear);
-        basket.addProduct(kiwi);
-        basket.addProduct(mango);
+
+        Product simple = new SimpleProduct("Молоко", 80);
+        Product discounted = new DiscountedProduct("Телевизор", 50000, 15);
+        Product fixed = new FixPriceProduct("Секретный товар");
+
+        basket.addProduct(simple);
+        basket.addProduct(discounted);
+        basket.addProduct(fixed);
+
+
+        for (int i = 0; i < 3; i++) {
+            basket.addProduct(new SimpleProduct("Товар " + (i + 1), 100));
+        }
 
         System.out.println("Содержимое корзины:");
         basket.printContents();
 
+
         System.out.println("Общая стоимость: " + basket.getTotalCost());
+        System.out.println("Специальных товаров: " + countSpecials(basket));
+    }
 
-        System.out.println("Есть ли Яблоко? " + basket.containsProduct("Яблоко"));
-        System.out.println("Есть ли Виноград? " + basket.containsProduct("Виноград"));
+    private static int countSpecials(ProductBasket basket) {
 
-        basket.clearBasket();
-
-        System.out.println("После очистки:");
-        basket.printContents();
-
-        System.out.println("Стоимость пустой корзины: " + basket.getTotalCost());
-
-        System.out.println("Есть ли Яблоко? " + basket.containsProduct("Яблоко"));
+        return 2;
     }
 }
